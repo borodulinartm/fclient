315a9b7f2d2e6f2cb674bc8d5a631efa4185817d
Artem <borodulin.artm@inbox.ru>
2022-02-18 19:48:03 +0000

    Making laba 1

---
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -0,0 +1,59 @@
+package ru.iu3.fclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import ru.iu3.fclient.databinding.ActivityMainBinding;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'RPO2022' library on application startup.
+    static {
+        System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int res = initRng();
+        byte[] keys = randomBytes(16);
+
+        Random random = new Random();
+
+        byte[] data = new byte[20];
+        for (int i = 0; i < data.length; ++i) {
+            data[i] = (byte) ((byte) random.nextInt() % 255);
+        }
+
+        // Пример шифрованя данных (в отладчике)
+        byte[] encrypt_data = encrypt(keys, data);
+
+        // Пример дешифрования данных (в отладчике)
+        byte[] decrypt_data = decrypt(keys, encrypt_data);
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'fclient' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+}
